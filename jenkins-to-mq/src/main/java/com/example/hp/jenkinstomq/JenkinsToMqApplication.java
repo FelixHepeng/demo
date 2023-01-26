7e814ab7b83bedf2cbe34b25c0378c7ca0bd91cc
FelixHepeng <1026964000@qq.com>
2023-01-26 12:07:29 +0800

    init

---
+++ jenkins-to-mq/src/main/java/com/example/hp/jenkinstomq/JenkinsToMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.hp.jenkinstomq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsToMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsToMqApplication.class, args);
+	}
+
+}
---
+++ jenkins-to-mq/src/test/java/com/example/hp/jenkinstomq/JenkinsToMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hp.jenkinstomq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsToMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
